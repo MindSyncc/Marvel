9589fb3ca2adeebcc21cfa031328a8a41ffef901
Fernando C <thelonder461@gmail.com>
2024-05-18 21:49:59 -0300

    Adicionando os primeiros produtos

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
+//ROLAR SLIDES AUTOMATICAMENTE
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){nextImage();    
+}, 5000)
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
      
     const email=document.querySelector(".email");
