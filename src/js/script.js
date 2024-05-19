0024a38a7593ee8cb0320efaf9bfb526eed49dd2
Fernando C <thelonder461@gmail.com>
2024-05-19 11:36:23 -0300

    adicionando o carrossel

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 //ROLAR SLIDES AUTOMATICAMENTE
+
 let count = 1;
 document.getElementById("radio1").checked = true;
 
@@ -11,6 +12,34 @@ function nextImage(){
     }
     document.getElementById("radio"+count).checked = true;
 }
+
+
+const track = document.getElementById("image-track");
+
+
+
+window.onmousedown = e => track.dataset.mouseDownAt = e.clientX;
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -60);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.style.transform = `translate(${nextPercentage}%, 0%)`;
+
+}
+
 
 document.addEventListener("DOMContentLoaded", function(){
      
@@ -30,3 +59,4 @@ document.addEventListener("DOMContentLoaded", function(){
 
     })
 })
+
